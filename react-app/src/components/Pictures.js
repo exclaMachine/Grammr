92ef192583ad588c518eda2ab14a454c423a1b5e
Ty <ty.rickers@gmail.com>
2022-04-12 16:39:50 -0600

    added a basic component for rendering all the users pics

---
+++ react-app/src/components/Pictures.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+const PicturesPage = () => {
+    const dispatch = useDispatch()
+    return (
+        <>
+        <div>
+            <h1>Pictures</h1>
+        </div>
+        </>
+    )
+}
