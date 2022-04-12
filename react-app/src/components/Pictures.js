44a95279c0cff8d642f8e8c1c132942b13ee3b84
Ty <ty.rickers@gmail.com>
2022-04-12 17:03:56 -0600

    added picture reducer to the root reducer

--- react-app/src/components/Pictures.js
+++ react-app/src/components/Pictures.js
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { getAllPicturesThunk } from '../store/picture'
+
 
 const PicturesPage = () => {
     const dispatch = useDispatch()
+
+    const sessionUser = useSelector(state => state.session.user)
+    const pictureObj = useSelector(state = state.pictureReducer)
+
+    console.log('obj', pictureObj)
+
+
+
 
     return (
         <>
