f9a33b4624b127a8b54d170dc469e94ad584ff79
Ty <ty.rickers@gmail.com>
2022-04-12 16:24:52 -0600

    just deleted version file and started over

--- app/models/db.py
+++ app/models/db.py
@@ -3,19 +3,19 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
-# class Picture(db.Model):
-#     __tablename__ = 'pictures'
+class Picture(db.Model):
+    __tablename__ = 'pictures'
 
-#     id = db.Column(db.Integer, primary_key=True, nullable=False)
-#     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-#     album_id = db.Column(db.Integer, db.ForeignKey("albums.id"), nullable=True)
-#     content = db.Column(db.String(255), nullable=False)
-#     image = db.Column(db.String(), nullable=False)
-#     created_at = db.Column(db.DateTime, default=datetime.now(), nullable=False)
-#     updated_at = db.Column(db.DateTime, default=datetime.now(), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    album_id = db.Column(db.Integer, db.ForeignKey("albums.id"), nullable=True)
+    content = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String(), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(), nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.now(), nullable=False)
 
-#     users = db.relationship("User", backref="pictures")
-#     comments = db.relationship("Comment", backref="pictures", cascade="all, delete", order_by="Comment.created_at")
+    users = db.relationship("User", backref="pictures")
+    comments = db.relationship("Comment", backref="pictures", cascade="all, delete", order_by="Comment.created_at")
 
 
 class Album(db.Model):
