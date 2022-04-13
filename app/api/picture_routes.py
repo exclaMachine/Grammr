9249c0259c8dbfdabd6e10e2b0b3ecdd6aa3992a
Ty <ty.rickers@gmail.com>
2022-04-13 10:33:49 -0600

    created route function. Change get all pics function to snake_case

--- app/api/picture_routes.py
+++ app/api/picture_routes.py
@@ -1,10 +1,42 @@
 from flask import Blueprint, request
 from app.models import db, Picture
+from flask_login import current_user, login_required
+from app.s3_helpers import (
+    upload_file_to_s3, allowed_file, get_unique_filename)
 
 picture_routes = Blueprint('pictures', __name__)
 
 @picture_routes.route('')
-def getPictures():
-    print('in the route')
+def get_pictures():
+    # print('in the route')
+    # pictures = Picture.query.order_by(Picture.id.desc()).all()
     pictures = Picture.query.all()
     return {'pictures': [picture.to_dict() for picture in pictures]}
+
+@picture_routes.route("", methods=["POST"])
+@login_required
+def upload_picture():
+    if "picture" not in request.files:
+        return {"errors": "image required"}, 400
+
+    image = request.files["picture"]
+
+    if not allowed_file(image.filename):
+        return {"errors": "file type not permitted"}, 400
+
+    image.filename = get_unique_filename(image.filename)
+
+    upload = upload_file_to_s3(image)
+
+    if "url" not in upload:
+        # if the dictionary doesn't have a url key
+        # it means that there was an error when we tried to upload
+        # so we send back that error message
+        return upload, 400
+
+    url = upload["url"]
+    # flask_login allows us to get the current user from the request
+    new_image = Picture(user=current_user, url=url)
+    db.session.add(new_image)
+    db.session.commit()
+    return {"url": url}
