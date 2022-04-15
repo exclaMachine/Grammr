0e860d682432049a9d7aef3d87f5fb3632e8d42c
Ty <ty.rickers@gmail.com>
2022-04-15 11:04:22 -0600

    created albums component

---
+++ react-app/src/components/Albums.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAllAlbumsThunk } from '../store/album'
+import DeletePicture from './DeletePic';
+import EditPicture from './EditPic';
+
+const AlbumsPage = () => {
+    const dispatch = useDispatch()
+
+    const sessionUser = useSelector(state => state.session.user)
+    const albumObj = useSelector(state => state.albumReducer)
+
+    // console.log('obj', albumObj)
+    let albums = Object.values(albumObj)
+
+    let usersAlbums = albums.filter(album => album?.user_id === sessionUser?.id)
+
+
+    useEffect(() => {
+        dispatch(getAllAlbumsThunk())
+    }, [dispatch])
+
+    return (
+        <>
+        <div>
+            <h1>Albums</h1>
+
+            <div className='albumsContainer'>
+                <div>
+                    <ul>
+                        {usersAlbums.map(({ id, title}) => (
+                            <li key={id}>
+                                {/* <EditPicture id={id}/> */}
+                                <h1>{title}</h1>
+                                {/* <DeletePicture id={id}/> */}
+                            </li>
+                        ))}
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
+        </>
+    )
+}
+
+export default AlbumsPage;
