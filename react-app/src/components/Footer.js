3030212b900cf089bd5ade7b9b0d0bb322231e9d
Ty <ty.rickers@gmail.com>
2022-04-16 09:27:14 -0600

    added links to github and linkedin

---
+++ react-app/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const FooterLinks = () => {
+
+    return(
+        <div className='footer'>
+             <ul>
+                    <li>
+                        <a style={{color:'rgb(61, 161, 255)'}} href='https://www.linkedin.com/in/ty-rickers-5b048b89/'>LinkedIn</a>
+                    </li>
+
+                    <li>
+                    <a style={{color:'rgb(61, 161, 255)'}} href='https://github.com/exclaMachine'>GitHub</a>
+                    </li>
+                </ul>
+        </div>
+    )
+}
+
+export default FooterLinks;
