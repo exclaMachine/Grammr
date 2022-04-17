6161ce6a7185a00353959606e2e946dbd1ce323a
Ty <ty.rickers@gmail.com>
2022-04-16 21:19:55 -0600

    seeded website with more pics

--- app/seeds/pictures.py
+++ app/seeds/pictures.py
@@ -6,7 +6,7 @@ today = date.today()
 
 pict_dict = [
     {
-        "id": 1,
+        "id": 2,
         "cont": "What!?",
         "img": "https://i0.wp.com/awsymbols.com/wp-content/uploads/2019/05/The_Punctuators_Super-Powered_Punctuation_Page-01-1.png",
         "created": today.strftime("%B %d, %Y"),
@@ -20,7 +20,7 @@ pict_dict = [
         "updated": today.strftime("%B %d, %Y")
     },
     {
-        "id": 2,
+        "id": 1,
         "cont": "Block Letters",
         "img": "https://images.unsplash.com/reserve/uZYSV4nuQeyq64azfVIn_15130980706_64134efc6e_o.jpg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z3JhbW1hcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
         "created": today.strftime("%B %d, %Y"),
@@ -32,7 +32,71 @@ pict_dict = [
         "img": "https://images.unsplash.com/photo-1543109740-4bdb38fda756?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Z3JhbW1hcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
         "created": today.strftime("%B %d, %Y"),
         "updated": today.strftime("%B %d, %Y")
-    }
+    },
+    {
+        "id": 2,
+        "cont": "Comma, Comma",
+        "img": "https://i0.wp.com/awsymbols.com/wp-content/uploads/2020/06/W052820_1.jpg?w=800&ssl=1",
+        "created": today.strftime("%B %d, %Y"),
+        "updated": today.strftime("%B %d, %Y")
+    },
+    {
+        "id": 2,
+        "cont": "Anagrams",
+        "img": "https://i0.wp.com/awsymbols.com/wp-content/uploads/2020/10/A101620_1.jpg?w=800&ssl=1",
+        "created": today.strftime("%B %d, %Y"),
+        "updated": today.strftime("%B %d, %Y")
+    },
+    {
+        "id": 2,
+        "cont": "Semordnilap",
+        "img": "https://i0.wp.com/awsymbols.com/wp-content/uploads/2020/10/A101620_2.jpg?w=800&ssl=1",
+        "created": today.strftime("%B %d, %Y"),
+        "updated": today.strftime("%B %d, %Y")
+    },
+    {
+        "id": 2,
+        "cont": "Interjection!",
+        "img": "https://i0.wp.com/awsymbols.com/wp-content/uploads/2020/11/0EEB01B4-DCED-4CDD-91F8-A79C6BD819A6.jpeg?w=800&ssl=1",
+        "created": today.strftime("%B %d, %Y"),
+        "updated": today.strftime("%B %d, %Y")
+    },
+    {
+        "id": 2,
+        "cont": "Then vs Than",
+        "img": "https://i0.wp.com/awsymbols.com/wp-content/uploads/2020/11/0023B6E8-7C78-4173-86FB-803B1E74BC4D.jpeg?w=800&ssl=1",
+        "created": today.strftime("%B %d, %Y"),
+        "updated": today.strftime("%B %d, %Y")
+    },
+    {
+        "id": 2,
+        "cont": "Apostrophantom",
+        "img": "https://i0.wp.com/awsymbols.com/wp-content/uploads/2021/01/1CB03FD4-6498-4A56-B317-E79200132B12.jpeg?w=800&ssl=1",
+        "created": today.strftime("%B %d, %Y"),
+        "updated": today.strftime("%B %d, %Y")
+    },
+    {
+        "id": 2,
+        "cont": "Conjunctions",
+        "img": "https://i0.wp.com/awsymbols.com/wp-content/uploads/2021/05/7954F0E2-C272-4B3A-908C-A9334BC69640.jpeg?w=800&ssl=1",
+        "created": today.strftime("%B %d, %Y"),
+        "updated": today.strftime("%B %d, %Y")
+    },
+    {
+        "id": 2,
+        "cont": "etc & et al",
+        "img": "https://i0.wp.com/awsymbols.com/wp-content/uploads/2021/10/AFC895C9-E5B9-4025-AAB4-1FB617227009.jpeg?w=800&ssl=1",
+        "created": today.strftime("%B %d, %Y"),
+        "updated": today.strftime("%B %d, %Y")
+    },
+    {
+        "id": 2,
+        "cont": "Ambigrams",
+        "img": "https://i0.wp.com/awsymbols.com/wp-content/uploads/2021/10/E6FF6D0B-7D5D-44CA-84E9-787F7833C470.png?w=800&ssl=1",
+        "created": today.strftime("%B %d, %Y"),
+        "updated": today.strftime("%B %d, %Y")
+    },
+
 ]
 
 def seed_pics():
