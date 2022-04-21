400ce4c063df88fb9b15701a4e4e76c84057dc3d
Ty <ty.rickers@gmail.com>
2022-04-21 12:15:58 -0600

    links now open in new tabs

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -5,11 +5,15 @@ const FooterLinks = () => {
     return(
         <div className='footer'>
                 <div>
-                    <a style={{color:'rgb(61, 161, 255)'}} href='https://www.linkedin.com/in/ty-rickers-5b048b89/'>LinkedIn</a>
+                    <a style={{color:'rgb(61, 161, 255)'}} href='https://www.linkedin.com/in/ty-rickers-5b048b89/' target="_blank">LinkedIn</a>
                 </div>
 
                 <div>
-                    <a style={{color:'rgb(61, 161, 255)'}} href='https://github.com/exclaMachine'>GitHub</a>
+                    <a style={{color:'rgb(61, 161, 255)'}} href='https://github.com/exclaMachine' target="_blank">GitHub</a>
+                </div>
+
+                <div>
+                    <a style={{color:'rgb(61, 161, 255)'}} href='https://www.webtoons.com/en/challenge/the-punctuators-super-powered-punctuation/list?title_no=318764' target="_blank">The Punctuators</a>
                 </div>
         </div>
     )
