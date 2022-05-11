d28327946d9a2b56ab1bc2f444683f7e4c6a44f7
Ty <ty.rickers@gmail.com>
2022-05-11 14:46:18 -0600

    created functions so delete buttons are hidden until the picture is hovered over

--- react-app/src/components/pictures/Pictures.js
+++ react-app/src/components/pictures/Pictures.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getAllPicturesThunk } from '../../store/picture'
@@ -11,12 +11,22 @@ import EditPicture from './EditPic';
 const PicturesPage = () => {
     const dispatch = useDispatch()
 
+    const [isHidden, setHidden] = useState(false)
+
+
     const sessionUser = useSelector(state => state.session.user)
     const pictureObj = useSelector(state => state.pictureReducer)
     let pictures = Object.values(pictureObj)
 
     let usersPictures = pictures.filter(picture => picture?.user_id === sessionUser?.id).reverse()
 
+    const Hide = () => {
+        setHidden(true);
+    }
+
+    const Show = () => {
+        setHidden(false);
+    }
 
     useEffect(() => {
         dispatch(getAllPicturesThunk())
@@ -34,16 +44,16 @@ const PicturesPage = () => {
                  <div>
                      <div className='pic-list'>
                          {usersPictures.map(({ id, content, image}) => (
-                             <div className="individual-pic" key={id}>
+                             <div onMouseEnter={Hide} onMouseLeave={Show} className="individual-pic" key={id}>
                                  <EditPicture id={id}/>
                                  {/* <EditPicModal id={id}/> */}
                                  {/* <h1>{content}</h1> */}
                                      <NavLink className="navBar" to={`/pictures/${id}`} exact={true} activeClassName='active'>
                                          <img alt='' src={image}></img>
                                      </NavLink>
-                                    <div>
-                                     <DeletePicModal id={id}/>
-                                     </div>
+                                        <div className={isHidden ? 'not-hidden': 'hidden'}>
+                                             <DeletePicModal id={id}/>
+                                        </div>
                              </div>
                          ))}
                      </div>
