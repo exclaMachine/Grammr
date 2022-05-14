975f776d5b3ac6f9617b1165f53fd22c1168637b
Ty <ty.rickers@gmail.com>
2022-05-14 09:47:30 -0600

    removed enterStrike function. Search bar now will go to page of pic. Chooses the lower number if they have the same name. Does not work it is on the single pic page

--- react-app/src/components/Searchbar.js
+++ react-app/src/components/Searchbar.js
@@ -1,44 +1,64 @@
-import React, { useState} from 'react'
+import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch} from 'react-redux';
+import { getAllPicturesThunk } from '../store/picture';
 //search bar should search for picture titles and then return single pic
 
 const SearchBar = () => {
-    const dispatch = useDispatch()
-
+    const dispatch = useDispatch();
+    const history = useHistory();
     const pictureObj = useSelector(state => state.pictureReducer)
+
+    useEffect(() => {
+        dispatch(getAllPicturesThunk())
+    }, [dispatch])
 
     let pictures = Object.values(pictureObj)
     console.log('pics for search', pictures)
 
     const [searchInput, setSearchInput] = useState('');
 
-    const handleSearch = (e) => {
-        e.preventDefault();
-        setSearchInput(e.target.value)
-
-    }
     console.log('search', searchInput)
 
 
-    const foundPic = pictures.find((pic) => {
-            return pic.content.toLowerCase() === searchInput.toLowerCase()
-        })
 
+    let foundPic = pictures.find((pic) => {
+        return pic.content.toLowerCase() === searchInput.toLowerCase()
+    })
 
+    const handleSearch = (e) => {
+        e.preventDefault();
 
+        if (foundPic?.id !== undefined) {
+            history.push(`/pictures/${foundPic.id}`)
+            setSearchInput('')
+        }
 
+    }
+
+    //by having this it was interfering with the other enter and causing problems
+    // let enterStrike = (e) => {
+    //     document.addEventListener('keypress', e => {
+    //         if (e.key === 'Enter') {
+    //             return handleSearch(e)
+    //         }
+    //     })
+
+    // }
 
 
     return (
         <>
+        <form onSubmit={handleSearch}>
             <input
             type='text'
-            onChange={handleSearch}
+            onChange={(e) => setSearchInput(e.target.value)}
             value={searchInput}
             />
 
-        <h1>{foundPic?.id}</h1>
-
+            <h1>{foundPic?.id}</h1>
+            <button type='submit'>Search</button>
+        </form>
         </>
     )
 }
